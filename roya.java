34b9c0c7f44f347caa49d51c2aa63efda309310f
royaso <zhangroyaso@gmail.com>
2016-08-27 16:16:39 +0800

    short code

---
+++ roya.java
@@ -0,0 +1,13 @@
+class roya{
+public String name;
+public static void main(String args[]){
+System.out.print("dasf");
+System.out.println("xxx");
+roya("uuuu");
+}
+
+public void roya(String name){
+name=name;
+System.out.println(name);
+}
+}
